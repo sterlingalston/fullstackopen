13de9e767cb3d084aca8cf4c70ee97f7703ffb65
Matthew Alston <sterlingalston@gmail.com>
2020-07-31 16:05:00 -0400

    completed part 2a rendering a collection, modules

---
+++ part2/2.1/src/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Course = (props) => {
+  console.log(props)
+  const { course } = props
+  return (
+    <div>
+    <h1>{course.name}</h1>
+
+    {course.parts.map((x) =>
+    	<div>{x.name} {x.exercises}</div>
+    	)}
+
+    	
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+
+ReactDOM.render(
+  <App  />,
+  document.getElementById('root')
+)
